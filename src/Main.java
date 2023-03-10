3f8bb6e1881b8e03266904fe45c8a59be8f5be30
Nikita Shulgin <shnn7997@gmail.com>
2023-03-10 15:28:52 +0700

    JDZ1-1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите n: ");
+        int n = scanner.nextInt();
+
+        // Вычисляем n-ое треугольное число
+        int triangleNumber = 0;
+        for (int i = 1; i <= n; i++) {
+            triangleNumber += i;
+        }
+        System.out.println("n-ое треугольное число: " + triangleNumber);
+
+        // Вычисляем факториал n
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        System.out.println("Факториал n: " + factorial);
+    }
+}
